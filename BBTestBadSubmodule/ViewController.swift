e6fb58accdf8b268c18ba12ee5fe11f73286b066
Christopher Stott <christopherstott@gmail.com>
2015-06-17 17:24:19 -0700

    Initial

---
+++ BBTestBadSubmodule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BBTestBadSubmodule
+//
+//  Created by Chris on 2015-06-17.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
